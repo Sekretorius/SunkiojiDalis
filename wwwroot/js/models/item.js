3edc1fa42a4e835cf8af5b1800e5e206620155d1
kardoka <v77@tuta.io>
2021-10-02 12:12:25 +0300

    Implement item class in JS

---
+++ wwwroot/js/models/item.js
@@ -0,0 +1,11 @@
+class Item {
+  constructor(id, name, weight, quantity, x, y, typeOfItem) {
+    this.id = id;
+    this.name = name;
+    this.weight = weight;
+    this.quantity = quantity;
+    this.x = x;
+    this.y = y;
+    this.typeOfItem = typeOfItem;
+  }
+}
