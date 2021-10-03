d6bddec813e6921f80ee612869814b3610626b67
kardoka <v77@tuta.io>
2021-10-03 20:15:10 +0300

    Implement item list

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -47,6 +47,66 @@ namespace SunkiojiDalis.Hubs
         public static Dictionary<int, Player> players = new Dictionary<int, Player>();
     }
 
+    public static class ItemsList {
+        public static Dictionary<int, Item> items = new Dictionary<int, Item>();
+        
+        public static Item GenerateItem() {
+            Random rd = new Random();
+            int randNum = rd.Next(1, 9);
+            int randomItemId = rd.Next(1, 99999);
+            AbstractItemFactory factory;
+            if(randNum > 7) {
+                factory = new LegendaryItemFactory();
+            } else {
+                factory = new CommonItemFactory();
+            }
+            string spritePath = "";
+            GenerateRandomItemAttributes(out int id, 
+                                         out string name,
+                                         out int weight,
+                                         out int quantity,
+                                         out int x,
+                                         out int y);
+            switch(randNum) {
+                case 1 or 2:
+                    spritePath = PickRandomItemSprite("resources/items/armor/armor");
+                    return factory.CreateArmor(id, spritePath, name, weight, quantity, x, y, rd.Next(1,11));
+                case 3 or 4:
+                    spritePath = PickRandomItemSprite("resources/items/weapon/weapon");
+                    return factory.CreateWeapon(id, spritePath, name, weight, quantity, x, y, rd.Next(1,11));
+                case 5 or 6:
+                    spritePath = PickRandomItemSprite("resources/items/weapon/weapon");
+                    return factory.CreateFood(id, spritePath, name, weight, quantity, x, y, rd.Next(1,101));
+                case 7 or 8:
+                    spritePath = PickRandomItemSprite("resources/items/weapon/weapon");
+                    return factory.CreatePotion(id, spritePath, name, weight, quantity, x, y, "Useless ability");
+                default:
+                    break;
+            }
+            var nullItem = factory.CreateArmor(-1, spritePath, "", -1, -1, -1, -1, -1);
+            return nullItem;
+        }
+
+        public static void GenerateRandomItemAttributes(out int id, out string name, out int weight, out int quantity, out int x, out int y) {
+            Random rd = new Random();
+            id = rd.Next(1, 99999);
+            while(items.ContainsKey(id)) {
+                id = rd.Next(1, 99999);
+            }
+            name = "item_" + id.ToString();
+            weight = rd.Next(1, 11);
+            quantity = rd.Next(1,4);
+            x = rd.Next(20, 780);
+            y = rd.Next(20, 480);
+        }
+
+        public static string PickRandomItemSprite(string prefix) {
+            Random rd = new Random();
+            string filename = prefix + (rd.Next(0, 10)).ToString() + ".png";
+            return filename;
+        }
+    }
+
     public class ChatHub : Hub
     {
         
