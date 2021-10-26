1b4ffee2c861f70e0888a670db96a23c1c013d54
kardoka <v77@tuta.io>
2021-10-27 02:58:34 +0300

    Update back-end items

--- Models/Items/Consumables/Foods/FoodRarities/CommonFood.cs
+++ Models/Items/Consumables/Foods/FoodRarities/CommonFood.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using SignalRWebPack.Engine;
+
 namespace SignalRWebPack{
   public class CommonFood: AbstractFood {
     public CommonFood(int id, string sprite, string name, int weight, int quantity, int x, int y, int belongsTo, int health) {
@@ -9,7 +12,14 @@ namespace SignalRWebPack{
       this.X = x;
       this.Y = y;
       this.BelongsTo = -1;
+      this.AreaId = AreaId;
       this.Health = health;
+    }
+    public override Dictionary<string, string> OnClientSideCreation() {
+      Dictionary<string, string> commonFoodData = base.OnClientSideCreation();
+      commonFoodData["health"] = this.Health.ToString();
+      commonFoodData["objectType"] = nameof(ServerObjectType.CommonFood);
+      return commonFoodData;
     }
   }
 }
