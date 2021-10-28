36052c83086dd11c1ce0480138c656408777594e
Paulius Balčiūnas <paulius.balciunas@sneakybox.lt>
2021-10-28 07:43:41 +0300

    added adapter

--- Hubs/ChatHub.cs
+++ Hubs/ChatHub.cs
@@ -179,11 +179,36 @@ namespace SignalRWebPack.Hubs
             {
                 PlayersList.players[rand_num] = convertedPlayer;
                 World.Instance.AddPlayer(PlayersList.players[rand_num]);
+
             }
             await Clients.Caller.SendAsync("RecieveId", Newtonsoft.Json.JsonConvert.SerializeObject(convertedPlayer.getId()));
             await Groups.AddToGroupAsync(Context.ConnectionId,convertedPlayer.GetGroupId());
             await Clients.Group(convertedPlayer.GetGroupId()).SendAsync("RecieveInfoAboutOtherPlayers", JsonConvert.SerializeObject(World.Instance.GetPlayers(convertedPlayer.worldX, convertedPlayer.worldY) ));
             await ServerEngine.NetworkManager.OnNewClientConnected(Clients.Caller);
+
+
+            lock(PlayerProccessLock)
+            {
+                ISaveFileAdapter xmlAdapter = new XMLAdapter(new XMLWritter());
+                ISaveFileAdapter txtAdapter = new TXTAdapter(new TXTWritter());
+                List<Player> players = new List<Player>(PlayersList.players.Values);
+
+                txtAdapter.Save(players);
+                xmlAdapter.Save(players);
+
+                List<string> data1 = xmlAdapter.Read();
+                List<string> data2 = txtAdapter.Read();
+
+                foreach(string p1 in data1)
+                {
+                    Console.WriteLine("XML DATA: " + p1);
+                }
+
+                foreach(string p2 in data2)
+                {
+                    Console.WriteLine("TXT DATA: " + p2);
+                }
+            }
         }
 
         public async Task MovePlayer(Player convertedPlayer, int worldX, int worldY, int x, int y)
